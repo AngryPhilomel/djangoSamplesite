8bbd9e6c4025c6fd0d93044c6e1276ac835af739
AngryPhilomel <an.aries@icloud.com>
2019-08-18 13:44:07 +0300

    1.15

--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,4 +1,7 @@
-from django.http import  HttpResponse
+from django.shortcuts import render
+
+from .models import Bb
 
 def index(request):
-    return HttpResponse("Скоро все будет!")
+    bbs = Bb.objects.all()
+    return render(request, 'bboard/index.html', {'bbs': bbs})
