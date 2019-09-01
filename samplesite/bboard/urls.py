758f60fe0c00a498ddbd56f38ad1cfaa9261eef3
AngryPhilomel <an.aries@icloud.com>
2019-09-01 17:27:54 +0300

    10.6

--- samplesite/bboard/urls.py
+++ samplesite/bboard/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import index, by_rubric, BbCreateView
+from .views import index, by_rubric, BbCreateView, BbDetailView, BbByRubricView, BbAddView, BbEditView, BbDeleteView
 
 urlpatterns = [
-    path('<int:rubric_id>/', by_rubric, name='by_rubric'),
+    path('<int:rubric_id>/', BbByRubricView.as_view(), name='by_rubric'),
+    path('add/', BbAddView.as_view(), name='add'),
+    path('detail/<int:pk>', BbDetailView.as_view(), name='detail'),
+    path('update/<int:pk>', BbEditView.as_view(), name='update'),
+    path('delele/<int:pk>', BbDeleteView.as_view(), name='delete'),
     path('', index, name='index'),
-    path('add/', BbCreateView.as_view(), name='add'),
 ]
--- samplesite/bboard/views.py
+++ samplesite/bboard/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, FormView, UpdateView, DeleteView
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.urls import reverse_lazy, reverse
 
 from .models import Bb, Rubric
 from .forms import BbForm
+
 
 def index(request):
     bbs = Bb.objects.all()
     rubrics = Rubric.objects.all()
     context = {'bbs': bbs, 'rubrics': rubrics}
     return render(request, 'bboard/index.html', context)
+
 
 def by_rubric(request, rubric_id):
     bbs = Bb.objects.filter(rubric=rubric_id)
@@ -18,6 +22,7 @@ def by_rubric(request, rubric_id):
     context = {'bbs': bbs, 'rubrics': rubrics, 'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
 
+
 class BbCreateView(CreateView):
     template_name = 'bboard/create.html'
     form_class = BbForm
@@ -25,5 +30,72 @@ class BbCreateView(CreateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+class BbDetailView(DetailView):
+    model = Bb
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubric'] = Rubric.objects.all()
+        return context
+
+
+class BbByRubricView(ListView):
+    template_name = 'bboard/by_rubric.html'
+    context_object_name = 'bbs'
+
+    def get_queryset(self):
+        return Bb.objects.filter(rubric=self.kwargs['rubric_id'])
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        context['current_rubric'] = Rubric.objects.get(pk=self.kwargs['rubric_id'])
+        return context
+
+
+class BbAddView(FormView):
+    template_name = 'bboard/create.html'
+    form_class = BbForm
+    initial = {'price': 0.0}
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+
+    def get_form(self, form_class=None):
+        self.object = super().get_form(form_class)
+        return self.object
+
+    def get_success_url(self):
+        return reverse('by_rubric',
+                       kwargs={'rubric_id': self.object.cleaned_data['rubric'].pk})
+
+
+class BbEditView(UpdateView):
+    model = Bb
+    form_class = BbForm
+    success_url = '/bboard'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+class BbDeleteView(DeleteView):
+    model = Bb
+    success_url = '/bboard'
+
+    def get_context_data(self,*args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
         context['rubrics'] = Rubric.objects.all()
         return context
